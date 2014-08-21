9280239acf69ebfd83b902b1bcbab9344126e38d
lynnaloo <lynnaloo@gmail.com>
2014-08-21 19:59:25 -0400

    edits to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
+# might not need sudo here if you run this as privileged in Vagrantfile
 sudo apt-get install git -y
 sudo apt-get install golang-go -y
-export PATH=$PATH:/usr/local/go/bin
 sudo apt-get install mercurial -y
+
+export PATH=$PATH:/usr/local/go/bin
