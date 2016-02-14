7ae5447ee3e0f152a6b4828abce6b53f883b6985
Linda Nichols <lynnaloo@gmail.com>
2016-02-14 18:33:40 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,8 @@ sudo apt-get install mercurial -y
 
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=/home/vagrant/dev
+
+go get golang.org/x/tools/cmd/godoc
+go get golang.org/x/tools/cmd/vet
+go get github.com/golang/lint/golint
 go get github.com/go-martini/martini
